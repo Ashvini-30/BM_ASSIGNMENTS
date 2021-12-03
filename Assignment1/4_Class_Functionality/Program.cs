44546d5968a4775a7d583194d2a9905d5d941c55
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-03 17:14:54 +0530

    Add files via upload

---
+++ Assignment1/4_Class_Functionality/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetProperty
+{
+    internal class Program
+    {
+        class Property
+        {
+
+            /* Program 4 -->
+             * Create class specified in example -3 using properties.
+            Call and verify class functionality using main method.*/
+
+            private int empNo;
+            private String eName;
+                private string designation;
+            private float salary;
+
+            public int EmpNo
+            {
+                get
+                {
+                    return empNo;
+                }
+
+                set
+                {
+                    empNo = value;
+                }
+            }
+
+            public string EName
+            {
+                get
+                {
+                    return eName;
+                }
+
+                set
+                {
+                    eName = value;
+                }
+            }
+
+            public string Designation { get; set; }
+
+
+
+
+            public float Salary
+            {
+                get
+                {
+                    return salary;
+                }
+
+                set
+                {
+                    salary = value;
+                }
+            }
+
+        }
+
+        
+        
+            static void Main(string[] args)
+            {
+                Property e = new Property();
+                e.EmpNo = 1;
+                e.EName = "ash";
+                e.Designation = "Manager";
+                Console.WriteLine("No = {0} Name :- {1}  Designation :- {2}", e.EmpNo, e.EName, e.Designation);
+
+                Console.ReadKey();
+            }
+        }
+
+
+    }
+
