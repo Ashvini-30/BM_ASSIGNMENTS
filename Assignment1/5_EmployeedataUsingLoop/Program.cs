6127e37a17fd547dc12f235f9921b9f564a877b6
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-03 17:16:22 +0530

    Add files via upload

---
+++ Assignment1/5_EmployeedataUsingLoop/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeArray
+{
+    class Employee
+    {
+        int empid;
+        string ename;
+        string designation;
+        int salary;
+
+        public Employee()
+        {
+            Console.WriteLine("Enter emp no");
+            empid = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter emp name");
+            ename = Console.ReadLine();
+
+            Console.WriteLine("Enter emp desg");
+            designation = Console.ReadLine();
+
+            Console.WriteLine("Enter emp salary");
+            salary = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void Main(string[] args)
+        {
+            Employee[] emp = new Employee[2];
+            for (int i = 0; i < emp.Length; i++)
+            {
+                emp[i] = new Employee();
+            }
+
+            foreach (Employee e in emp)
+            {
+                Console.WriteLine("EmpNo:" + e.empid + " EmpName:" + e.ename + " Desg:" + e.designation + " Salary:Rs." + e.salary);
+            }
+        }
+
+    }
+}
