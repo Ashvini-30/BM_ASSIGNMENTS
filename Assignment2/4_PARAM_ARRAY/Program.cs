66fd99cc768fc1e6dfc65336fe9b680071d99459
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-03 17:40:12 +0530

    Add files via upload

---
+++ Assignment2/4_PARAM_ARRAY/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamArray
+{
+    class Program
+    {
+
+
+
+        public static int Average(params int[] no)
+        {
+            int sum = 0, avg = 0;
+            for (int i = 0; i < no.Length; i++)
+            {
+                sum = sum + no[i];
+            }
+
+            avg = sum / no.Length;
+            return avg;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the average of given no is"+(Average(1, 2, 3)));
+            Console.ReadKey();
+        }
+    }
+}
