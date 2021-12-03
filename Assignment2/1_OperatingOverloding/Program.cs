db4deb3b1f8d544209ee871b02da5e9594a1ac4d
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-03 17:35:13 +0530

    Add files via upload

---
+++ Assignment2/1_OperatingOverloding/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloding
+{
+    class Student
+    {
+        int marks;
+        public Student(int m)
+        {
+            marks = m;
+        }
+        public static Student operator +(Student a, Student b)
+        {
+            Student res = new Student(0);
+            res.marks = a.marks + b.marks;
+            return res;
+
+        }
+        public static Student operator - (Student a,Student b)
+            {
+            Student res = new Student(0);
+              res.marks = a.marks - b.marks;
+            return res;
+
+            }
+        public static Student operator *(Student a,Student b)
+        {
+            Student res = new Student(1);
+            res.marks = a.marks * b.marks;
+            return res;
+        }
+        public static Student operator /(Student a,Student b)
+        {
+            Student res = new Student(1);
+            res.marks = a.marks / b.marks;
+            return res;
+        }
+        public static  bool operator ==(Student a,Student b)
+        {
+            if(a.marks==b.marks)
+             return true;
+             else
+               return false;
+
+            
+        }
+        public static bool operator !=(Student a, Student b)
+        {
+            if (a.marks != b.marks)
+                return true;
+            else
+                return false;
+
+
+        }
+        public void printmarks()
+        {
+            Console.WriteLine(marks);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj1 = new Student(100);
+            Student obj2 = new Student(50);
+            Student addres = obj1 + obj2;
+            addres.printmarks();
+        Student subres = obj1 - obj2;
+        subres.printmarks();
+            Student mulres = obj1 * obj2;
+            mulres.printmarks();
+            Student divres = obj1 / obj2;
+            divres.printmarks();
+          if(obj1==obj2)
+                Console.WriteLine("Marks are equal");
+            else
+                Console.WriteLine("Marks are not equal");
+            Console.ReadKey();
+        }
+    }
+}
