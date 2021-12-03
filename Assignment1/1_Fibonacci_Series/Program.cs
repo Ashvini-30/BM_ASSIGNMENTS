2ac02089fe84b793a8af3d30594de9521b86cbc1
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-03 17:06:47 +0530

    Add files via upload

---
+++ Assignment1/1_Fibonacci_Series/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,sum=0;
+            int a = 0;
+            int b = 1;
+            Console.WriteLine("Enter the no up to which u want to print fib series");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(sum<=n)
+            {
+                Console.Write(sum + " ");
+                a = b;
+                b = sum;
+                sum = a + b;
+
+            }
+            Console.ReadKey()
+;
+            
+
+        }
+    }
+}
