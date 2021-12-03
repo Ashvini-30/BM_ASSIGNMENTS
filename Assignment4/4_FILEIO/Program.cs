e8ca07afc659d94b2bb2f8bf5974e25b35d2ab64
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-03 17:03:24 +0530

    Add files via upload

---
+++ Assignment4/4_FILEIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IODemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("C:\\Users\\Jay Gajanan\\Documents\\Benchmark_Training.net\\IODemo");
+            if(di.Exists)
+            {
+                Console.WriteLine(di.FullName);
+                Console.WriteLine(di.CreationTime);
+                Console.WriteLine(di.Parent);
+            }
+            else
+            {
+                di.Create();
+                Console.WriteLine("Directory created");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
