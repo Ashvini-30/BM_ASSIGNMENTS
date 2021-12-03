7fe106a7e0009a5a690ace3b2e5cfd479c30ea9d
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-03 17:01:49 +0530

    Add files via upload

---
+++ Assignment4/3_Generic_Method_Swap/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethodSwapPrg
+{
+    class Program
+    {
+        public static void swap<T>(ref T a, ref T b)
+        {
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 30, b = 40;
+            char ch1 = 'x', ch2 = 'y';
+
+            Console.WriteLine("Before Swap the values are as follows");
+            Console.WriteLine("a= " + a + " b=" + b);
+            Console.WriteLine("ch1= " + ch1 + "  ch2=" + ch2);
+            swap(ref a, ref b);
+            swap(ref ch1, ref ch2);
+
+           
+            Console.WriteLine("after Swap the values are as follows");
+            Console.WriteLine("a= " + a + " b=" + a);
+            Console.WriteLine("ch1= " + ch1 + "  ch2=" + ch2);
+            Console.ReadKey();
+
+        }
+       
+    }
+}
