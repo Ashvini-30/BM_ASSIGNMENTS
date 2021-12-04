987b81fe5bae0350475ef91f8f1f46d3bd0fe705
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-04 15:56:12 +0530

    Add files via upload

---
+++ WebAPI/ServerConsumerAPI/ConsumerAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WEBAPIConsumer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebAPI/ServerConsumerAPI/ConsumerAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WEBAPIConsumer.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
