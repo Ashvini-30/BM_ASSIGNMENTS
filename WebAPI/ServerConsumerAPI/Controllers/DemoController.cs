f2a37c24ded99ab538fe661ad57c63e9e4e0702f
Ashvini-30 <76484495+Ashvini-30@users.noreply.github.com>
2021-12-04 15:32:24 +0530

    Add files via upload

---
+++ WebAPI/ServerConsumerAPI/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class DemoController : ApiController
+    {
+        /*public string Get()
+        {
+            return "Welcome to my WEB API";
+        }*/
+       public List<string> Get()
+        {
+            List<string> elist = new List<string>();
+                elist.Add("data1");
+            elist.Add("data2");
+            return elist;
+        }
+    }
+}
